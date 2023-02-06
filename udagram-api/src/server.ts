180e85b54af1ab7d11f5453df7071a4f473e137e
yasmeenturky <yasmeenahmed994@gmail.com>
2023-02-06 02:13:35 +0200

    build errors fix

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -19,13 +19,13 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
   }
 
   await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
+  await sequelize.addModels(V0_USER_MODELS);/*  */
   await sequelize.sync();
 
   console.log("Database Connected");
 
   const app = express();
-  const port = process.env.URL || 8080;
+  const port = process.env.PORT || 8080;
 
   app.use(bodyParser.json());
 
