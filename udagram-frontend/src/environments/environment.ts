5aa5dd5eda8550d5669de959fb1095b426d883c0
yasmeenturky <yasmeenahmed994@gmail.com>
2023-02-07 19:27:04 +0200

    deploy fix

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-dj5v4zsu.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-dev.eba-2pvteceu.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
